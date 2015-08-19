c53432ab9d119cee83a85bd95cb151a5568e43b1
Timothy Tsai <timothytsai@hsl.harvard.edu>
2015-08-19 17:49:25 -0400

    added commenting to pround.r (TOOLS-9), added median functionality to sum.stat

--- R/pround.r
+++ R/pround.r
@@ -1,10 +1,14 @@
 #' Pround
 #' @author TGT
 #' @export
+# rounds p-values to a number of digits which depends on the value of p
 
 pround<-function(p){
   r <- NULL
+# two digits for p >= 0.06
   if (p >= 0.06) r<-2
+  
+# 3 digits for 0.001 <= p < 0.06  
   if (p >= 0.001 & p < 0.06) r <- 3
   if(!is.null(r)) out<-formatC(p, digits=r, format="f", drop0trailing=F)
   if (p > 0.99) out <-  "> 0.99"
--- R/sum.stat.r
+++ R/sum.stat.r
@@ -2,16 +2,18 @@
 #' @author TGT
 #' @export
 
-sum.stat<-function(a, n=F, mu=F, meansd=F, s=F, q=NULL, type=8){
+sum.stat <- function(a, n=F, mu=F, med=F, meansd=F, s=F, q=NULL, type=8){
   # computes summary statistics based on requests
-  n<-switch(2-as.numeric(n), length(na.omit(a)), NULL)
-  mu<-switch(2-as.numeric(mu), mean(a, na.rm=T), NULL)
-  s<-switch(2-as.numeric(s), sd(a, na.rm=T), NULL)
-  quants<-switch(2-!is.null(q), quantile(a, q, na.rm=T, type=type), NULL)
-  pctround<-function(pct) ifelse(pct==0, 0, round(pct)*(pct>=1) + round(pct, digits=1)*(pct>=.1 & pct < 1) + round(pct, digits=2)*(pct>=.01 & pct < .1) + round(pct, digits=3)*(pct>=0 & pct < .01))
+  n <- switch(2-as.numeric(n), length(na.omit(a)), NULL)
+  mu <- switch(2-as.numeric(mu), mean(a, na.rm=T), NULL)
+  med <- switch(2-as.numeric(med), median(a, na.rm=T), NULL)
+  s <- switch(2-as.numeric(s), sd(a, na.rm=T), NULL)
+ 
+  quants <- switch(2-!is.null(q), quantile(a, q, na.rm=T, type=type), NULL)
+  pctround <- function(pct) ifelse(pct==0, 0, round(pct)*(pct>=1) + round(pct, digits=1)*(pct>=.1 & pct < 1) + round(pct, digits=2)*(pct>=.01 & pct < .1) + round(pct, digits=3)*(pct>=0 & pct < .01))
   if(length(na.omit(unique(a))) == 2){
     mu <- sum(a==1)
     s <- pctround(100*mean(a==1, na.rm=T))
   }
-  c(n, mu, s, quants) 
+  c(n, mu, med, s, quants) 
 }
